63a9335e8333bdcd7d598399f8edf33b4a45f54e
Sirianne-sys <56313116+Sirianne-sys@users.noreply.github.com>
2022-10-08 21:46:14 +0200

    1er code

---
+++ ex_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+int a=10,b=13,c;
+c=a+b;
+printf("the sum of %d and %d is %d\n",a,b,c);
+printf("the sum of %d and %d is %d\n",a,b,a+b);
+
+
+return 0;
+}
